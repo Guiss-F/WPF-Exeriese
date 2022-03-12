c67ad79745a23cf9b9b311db3a2f1171bebc6067
Guiss-F <56425338+Guiss-F@users.noreply.github.com>
2022-03-12 18:48:32 +0800

    Add files via upload

---
+++ Fast-Learning/GS.Music/MusicViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GS.Music
+{
+    public class MusicViewModel
+    {
+        #region Account
+        private string _account;
+
+        public string Account
+        {
+            get { return _account; }
+            set
+            {
+                _account = value;
+            }
+        }
+
+        #endregion
+
+        #region PassWard
+        private string _passWard;
+
+        public string PassWard
+        {
+            get { return _passWard; }
+            set { _passWard = value; }
+        }
+
+        #endregion
+
+        #region LoginTypes
+        private List<string> _loginTypes;
+        
+        public List<string> LoginTypes
+        {
+            get { return _loginTypes; }
+            set { _loginTypes = value; }
+        }
+        #endregion
+
+        public MusicViewModel()
+        {
+            Account = "Admin";
+            LoginTypes = new List<string>();
+            LoginTypes.Add("手机号登录");
+            LoginTypes.Add("密码登录");
+            LoginTypes.Add("二维码登录");
+        }
+    }
+
+}
