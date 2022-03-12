f74f2a5d8ac303589676d14fe1fd3d712c08b6c0
Guiss-F <136.75818169@163.com>
2022-03-13 02:12:17 +0800

    Add RelayCommand

--- Fast-Learning/GS.Music/MusicViewModel.cs
+++ Fast-Learning/GS.Music/MusicViewModel.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Gs.Common;
 
 namespace GS.Music
 {
@@ -43,13 +46,48 @@ namespace GS.Music
         }
         #endregion
 
+        public ICommand LoginCommand
+        {
+            get;
+            set;
+        }
+
+        private ICommand _cancelRemberCommand;
+
+        public ICommand CancelRemberCommand
+        {
+            get
+            {
+                return null == _cancelRemberCommand ?
+                    new RelayCommand<bool>(CancelAction) : _cancelRemberCommand; 
+            }
+        }
+
         public MusicViewModel()
         {
+            LoginCommand = new RelayCommand(LoginAction);
             Account = "Admin";
             LoginTypes = new List<string>();
             LoginTypes.Add("手机号登录");
             LoginTypes.Add("密码登录");
             LoginTypes.Add("二维码登录");
+        }
+
+        private void LoginAction()
+        {
+            if (Account == "Admin" && PassWard == "123456")
+            {
+                MessageBox.Show("登录成功！");
+            }
+            else
+            {
+                MessageBox.Show("登录失败！");
+            }
+        }
+
+        private void CancelAction(bool obj)
+        {
+            MessageBox.Show($"check status {obj}");
         }
     }
 
